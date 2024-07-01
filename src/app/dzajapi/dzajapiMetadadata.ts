77debddd60916b250c09a7ffc8dde6319e1ca6b8
Edson Rodrigo <63821150+Edson1337@users.noreply.github.com>
2024-07-01 19:03:59 -0300

    Update

---
+++ src/app/dzajapi/dzajapiMetadadata.ts
@@ -0,0 +1,12 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: "Dzaj API",
+	description: "An API documentation",
+	alternates: {
+		canonical: `/dzajapi`,
+		languages: {
+			"en-US": `/dzajapi`,
+		},
+	},
+};
---
+++ src/app/privacy-policy/privacyPolicyMetadata.tsx
@@ -0,0 +1,12 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: "Privacy Policy",
+	description: "A Privacy Policy",
+	alternates: {
+		canonical: `/privacy-policy`,
+		languages: {
+			"en-US": `/privacy-policy`,
+		},
+	},
+};
